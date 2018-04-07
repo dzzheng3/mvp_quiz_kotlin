087e80a6cdf3206772159be1593e623241fb844d
dz <dz@dzs-MacBook-Pro.local>
2018-04-06 23:14:11 -0700

    add splash

--- app/src/main/java/com/zheng/home/features/splash/WelcomeActivity.kt
+++ app/src/main/java/com/zheng/home/features/splash/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import com.jakewharton.rxbinding2.view.RxView
 import com.zheng.home.R
 import com.zheng.home.features.quiz.QuizActivity
@@ -15,7 +16,7 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         bt_start.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
